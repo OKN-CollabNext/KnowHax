bd0d8721f6618fe998356e7e213a1e8841e8219d
Luis Arias <luis@inclusif.eu>
2024-04-11 19:25:26 +0200

    Simplify front page

--- CollabNext/CollabNext.py
+++ CollabNext/CollabNext.py
@@ -1,8 +1,8 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
-from rxconfig import config
-
 import reflex as rx
+
+from rxconfig import config
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction/"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -16,13 +16,9 @@ def index() -> rx.Component:
     return rx.center(
         rx.theme_panel(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
+            rx.heading("Welcome to Collab Next!", size="9"),
+            rx.text("Search for anything"),
+            rx.input(),
             rx.logo(),
             align="center",
             spacing="7",
